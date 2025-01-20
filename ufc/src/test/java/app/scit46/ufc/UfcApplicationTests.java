7037dbd5aedafd9c6b3202bb7e97eb2a1907e176
Sorak <axsd789@gmail.com>
2025-01-20 18:57:17 +0900

    init

---
+++ ufc/src/test/java/app/scit46/ufc/UfcApplicationTests.java
@@ -0,0 +1,13 @@
+package app.scit46.ufc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
