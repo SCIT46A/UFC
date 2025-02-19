3119f5b0e2f02da805d75b741e5945d402bd571e
sorakazae <axsd789@gmail.com>
2025-02-19 18:06:59 +0900

    캠페인 컨트롤러 리퀘스트맵핑 복구구

--- ufc/src/main/java/app/scit46/ufc/controller/CampaignController.java
+++ ufc/src/main/java/app/scit46/ufc/controller/CampaignController.java
@@ -8,14 +8,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import app.scit46.ufc.dto.CampaignDTO;
 import app.scit46.ufc.service.campaign.CampaignService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 
 @Controller
+@RequestMapping("/campaign")
 @RequiredArgsConstructor
 public class CampaignController {
 
@@ -41,8 +45,14 @@ public class CampaignController {
     }
 
     @GetMapping("/pay")
-    public String payCampaign(@AuthenticationPrincipal UserDetails userDetails, Model model) {
-        String username = userDetails.getUsername(); // getUserName() 을 따로 만들지 고민 필요(사용자 이름)
+    public String payCampaign(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false); // 세션 가져오기
+        Long loginUserId = null; // 기본값 설정
+
+        if (session != null) {
+            loginUserId = (Long) session.getAttribute("loginUserId"); // 세션이 존재할 때만 값 가져오기
+        }
+        String username = request.getUser(); // getUserName() 을 따로 만들지 고민 필요(사용자 이름)
         model.addAttribute("username", username);
         return "campaign/pay-campaign";
     }
