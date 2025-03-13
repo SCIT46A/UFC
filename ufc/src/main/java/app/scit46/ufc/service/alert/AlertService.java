44bb36351dd3118ec0c0218120ac199b75d75421
sorakazae <axsd789@gmail.com>
2025-03-13 21:50:30 +0900

    알림 로직 작성중(대상 유저 추출 필요)

--- ufc/src/main/java/app/scit46/ufc/service/alert/AlertService.java
+++ ufc/src/main/java/app/scit46/ufc/service/alert/AlertService.java
@@ -1,5 +1,6 @@
 package app.scit46.ufc.service.alert;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,149 +20,208 @@ import app.scit46.ufc.entity.product.ProductEntity;
 import app.scit46.ufc.entity.reward.RewardDeliveryEntity;
 import app.scit46.ufc.repository.LikeRepository;
 import app.scit46.ufc.repository.alert.AlertRepository;
+import app.scit46.ufc.service.ImageUrlService;
+import app.scit46.ufc.service.cloudflare.ImageService;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AlertService {
     private final AlertRepository alertRepository;
-    
+    private final ImageService imageService;
+    private final ImageUrlService imageUrlService;
+    private final UserAlertService userAlertService;
 
     private AlertEntity createAlert(AlertEntity alert) {
         return alertRepository.save(alert);
     }
-    
+
     // public List<AlertDTO> getAlertList(Long userId) {
-        // 사용자 아이디를 기반으로 해당 사용자에 대한 모든 알림을 조회
-        // List<AlertEntity> alertList = alertRepository.findByUser_UserId(userId);
+    // 사용자 아이디를 기반으로 해당 사용자에 대한 모든 알림을 조회
+    // List<AlertEntity> alertList = alertRepository.findByUser_UserId(userId);
 
-        // // 사용자가 관심을 표한 창작자, 캠페인, 상품 조회 -> 각각 List로 추출
-        // List<LikeEntity> likeList = likeRepository.findByUser_UserId(userId);
-        // List<CreatorEntity> creatorList = likeList.stream().map(LikeEntity::getCreator).collect(Collectors.toList());
-        // List<CampaignEntity> campaignList = likeList.stream().map(LikeEntity::getCampaign).collect(Collectors.toList());
-        // List<ProductEntity> productList = likeList.stream().map(LikeEntity::getProduct).collect(Collectors.toList());
+    // // 사용자가 관심을 표한 창작자, 캠페인, 상품 조회 -> 각각 List로 추출
+    // List<LikeEntity> likeList = likeRepository.findByUser_UserId(userId);
+    // List<CreatorEntity> creatorList =
+    // likeList.stream().map(LikeEntity::getCreator).collect(Collectors.toList());
+    // List<CampaignEntity> campaignList =
+    // likeList.stream().map(LikeEntity::getCampaign).collect(Collectors.toList());
+    // List<ProductEntity> productList =
+    // likeList.stream().map(LikeEntity::getProduct).collect(Collectors.toList());
 
-        // AlertDTO alertSendDTO = AlertDTO.builder()
-        // .message(alertList.getMessage())
-        // .imageUrl(alertList.getImageUrl())
-        // .linkUrl(alertList.getLinkUrl())
-        // .receivedAt(alertList.getReceivedAt())
-        // .build();
-        // return alertList;
+    // AlertDTO alertSendDTO = AlertDTO.builder()
+    // .message(alertList.getMessage())
+    // .imageUrl(alertList.getImageUrl())
+    // .linkUrl(alertList.getLinkUrl())
+    // .receivedAt(alertList.getReceivedAt())
+    // .build();
+    // return alertList;
     // }
 
     // 알림 등록 / data는 각종 Entity 클래스 인스턴스
-    public void registAlert(Object data) {
-        if(data instanceof NoticeEntity) {  // 공지사항
+    public void registAlert(Object data, String type) {
+        AlertEntity alert;
+        // Admin 공지사항 등록 -> 전체유저
+        if (data instanceof NoticeEntity && type.equals("Notice")) {
             NoticeEntity notice = (NoticeEntity) data;
-            AlertEntity alert = AlertEntity.builder()
-            .content(generateMessage(notice, "notice"))
-            .alertType("notice")
-            .build();
-            createAlert(alert);
-        }else if(data instanceof CampaignEntity) {
+            alert = generateAlert(notice, "Notice");
+            
+        // 캠페인 등록 -> 관심유저, 캠페인 승인 / 거절 -> 창작자
+        } else if (data instanceof CampaignEntity) {
             CampaignEntity campaign = (CampaignEntity) data;
-            AlertEntity alert = AlertEntity.builder()
-            .content(generateMessage(campaign, "campaign"))
-            .alertType("campaign")
-            .build();
-            createAlert(alert);
-        }else if(data instanceof  CampaignBoardEntity) {
+            
+            if(type.equals("Regist")){
+                // 캠페인 등록시
+                alert = generateAlert(campaign, "Regist");
+            }else if(type.equals("Accept")){
+                // 캠페인 승인시
+                alert = generateAlert(campaign, "Accept");
+            }else if(type.equals("Decline")){
+                // 캠페인 거절시
+                alert = generateAlert(campaign, "Decline");
+            }else{
+                throw new IllegalArgumentException("[캠페인 알림등록]지원하지 않는 타입 : " + type);
+            }
+            
+        // 새 캠페인 소식 등록 -> 관심유저, 참여유저
+        } else if (data instanceof CampaignBoardEntity) {
             CampaignBoardEntity campaignBoard = (CampaignBoardEntity) data;
-            AlertEntity alert = AlertEntity.builder()
-            .content(generateMessage(campaignBoard, "campaignBoard"))
-            .alertType("campaignBoard")
-            .build();
-            createAlert(alert);
-        }else if(data instanceof BadgeEntity) {
-            BadgeEntity badge =  (BadgeEntity) data;
-            AlertEntity alert = AlertEntity.builder()
-            .content(generateMessage(badge, "badge"))
-            .alertType("badge")
-            .build();
-            createAlert(alert);
-        }else if(data instanceof MaterialDonationEntity) {
+            alert = generateAlert(campaignBoard, "Notice");
+
+        // 업적 달성 -> 전체유저(트리거?) / 업적 메시지(Alert)는 사전에 생성 후 업적 달성시 UserAlert에 저장
+        } else if (data instanceof BadgeEntity) {
+            BadgeEntity badge = (BadgeEntity) data;
+            alert = generateAlert(badge, "Badge");
+
+        // 캠페인 참여(펀딩) -> 창작자, 참여 승인 / 거절 -> 참여유저
+        } else if (data instanceof MaterialDonationEntity) {
             MaterialDonationEntity materialDonation = (MaterialDonationEntity) data;
-            AlertEntity alert = AlertEntity.builder()
-            .content(generateMessage(materialDonation, "materialDonation"))
-            .alertType("materialDonation")
-            .build();
-            createAlert(alert);
-        }else if(data instanceof ProductEntity) {
+            if(type.equals("Regist")){
+                // 참여 등록시
+                alert = generateAlert(materialDonation, "Regist");
+            }else if(type.equals("Accept")){
+                // 참여 승인시
+                alert = generateAlert(materialDonation, "Accept");
+            }else if(type.equals("Decline")){
+                // 참여 거절시
+                alert = generateAlert(materialDonation, "Decline");
+            }else{
+                throw new IllegalArgumentException("[참여 알림등록]지원하지 않는 타입 : " + type);
+            }
+
+        // 상품 등록 -> 창작자 관심유저
+        } else if (data instanceof ProductEntity) {
             ProductEntity product = (ProductEntity) data;
-            AlertEntity alert = AlertEntity.builder()
-            .content(generateMessage(product, "product"))
-            .alertType("product")
-            .build();
-            createAlert(alert);
+            alert = generateAlert(product, "Regist");
+
+        } else {
+            throw new IllegalArgumentException("[알림등록]지원하지 않는 데이터 타입 : " + data.getClass().getSimpleName());
         }
-        else{ 
-            throw new IllegalArgumentException("지원하지 않는 데이터 타입 : " + data.getClass().getSimpleName());
-        }
+
+        alertRepository.save(alert);
+        userAlertService.sendAlert(type, alert, null);
+        // user list 추출 필요
     }
 
+    // 알림 생성
+    public AlertEntity generateAlert(Object data, String type){
+        return AlertEntity.builder()
+                .content(generateMessage(data, type))
+                .alertType(type)
+                .imageUrl(imageService.getImageUrl(generateImageUrl(data))) // 0번이 기본 이미지
+                .linkUrl(generateLinkUrl(data, type))
+                .alertDate(LocalDateTime.now())
+                .build();
+    }
+
+    // 알림 메시지 생성
     public String generateMessage(Object data, String type) {
         String message = "";
-        if(data instanceof NoticeEntity) {
+        if (data instanceof NoticeEntity) {
             NoticeEntity notice = (NoticeEntity) data;
-            message = "새 공지사항 : "+notice.getTitle();
-        }else if(data instanceof CampaignEntity) {
+            message = "새 공지사항 : " + notice.getTitle();
+        } else if (data instanceof CampaignEntity) {
             CampaignEntity campaign = (CampaignEntity) data;
-            message = "새 캠페인 : "+campaign.getTitle();
-        }else if(data instanceof CampaignBoardEntity) {
+            message = "새 캠페인 : " + campaign.getTitle();
+        } else if (data instanceof CampaignBoardEntity) {
             CampaignBoardEntity campaignBoard = (CampaignBoardEntity) data;
-            message = "캠페인 새 소식 : "+campaignBoard.getTitle();
-        }else if(data instanceof BadgeEntity) {
+            message = "캠페인 새 소식 : " + campaignBoard.getTitle();
+        } else if (data instanceof BadgeEntity) {
             BadgeEntity badge = (BadgeEntity) data;
-            message = "새 업적 : "+badge.getName();
-        }else if(data instanceof MaterialDonationEntity) {
+            message = "새 업적 : " + badge.getName();
+        } else if (data instanceof MaterialDonationEntity) {
             MaterialDonationEntity materialDonation = (MaterialDonationEntity) data;
-            message = "새 자원 기부 : "+materialDonation.getCampaign().getTitle();
-        }else if(data instanceof ProductEntity) {
+            message = "새 자원 기부 : " + materialDonation.getCampaign().getTitle();
+        } else if (data instanceof ProductEntity) {
             ProductEntity product = (ProductEntity) data;
-            message = "새 상품 : "+product.getItem().getName();
-        }else if(data instanceof RewardDeliveryEntity) {
+            message = "새 상품 : " + product.getItem().getName();
+        } else if (data instanceof RewardDeliveryEntity) {
             RewardDeliveryEntity rewardDelivery = (RewardDeliveryEntity) data;
-            message = "리워드 배송 준비 : "+rewardDelivery.getInvoice();
-        }else if(data instanceof ProductDeliveryEntity) {
+            message = "리워드 배송 준비 : " + rewardDelivery.getInvoice();
+        } else if (data instanceof ProductDeliveryEntity) {
             ProductDeliveryEntity productDelivery = (ProductDeliveryEntity) data;
-            message = "상품 배송 준비 : "+productDelivery.getInvoice();
+            message = "상품 배송 준비 : " + productDelivery.getInvoice();
         }
 
-        else{
-            throw new IllegalArgumentException("지원하지 않는 데이터 타입 : " + data.getClass().getSimpleName());
+        else {
+            throw new IllegalArgumentException("[메시지 생성]지원하지 않는 데이터 타입 : " + data.getClass().getSimpleName());
         }
         return message;
     }
 
+    // 알림 이동 링크 URL 생성
     public String generateLinkUrl(Object data, String type) {
-        String linkUrl = "";
-        if(data instanceof NoticeEntity) {
+        String linkUrl = "https://upda.store";
+        if (data instanceof NoticeEntity) {
             NoticeEntity notice = (NoticeEntity) data;
-            linkUrl += "/notice/"+notice.getNoticeId();
-        }else if(data instanceof CampaignEntity) {
+            linkUrl = "";
+            //linkUrl += "/notice/" + notice.getNoticeId();
+        } else if (data instanceof CampaignEntity) {
             CampaignEntity campaign = (CampaignEntity) data;
-            linkUrl += "/campaign/"+campaign.getCampaignId();
-        }else if(data instanceof CampaignBoardEntity) {
+            linkUrl += "/campaign/" + campaign.getCampaignId();
+        } else if (data instanceof CampaignBoardEntity) {
             CampaignBoardEntity campaignBoard = (CampaignBoardEntity) data;
-            linkUrl += "/campaign/"+campaignBoard.getCampaign().getCampaignId();
-        }else if(data instanceof BadgeEntity) {
-            BadgeEntity badge = (BadgeEntity) data;
-            linkUrl += "/badge/"+badge.getBadgeId();
-        }else if(data instanceof MaterialDonationEntity) {
+            linkUrl += "/campaign/" + campaignBoard.getCampaign().getCampaignId();
+        } else if (data instanceof BadgeEntity) {
+            //BadgeEntity badge = (BadgeEntity) data;
+            linkUrl += "/user/badge";
+        } else if (data instanceof MaterialDonationEntity) {
             MaterialDonationEntity materialDonation = (MaterialDonationEntity) data;
-            linkUrl += "/campaign/"+materialDonation.getCampaign().getCampaignId();
-        }else if(data instanceof ProductEntity) {
-            ProductEntity product = (ProductEntity) data;
-            linkUrl += "/product/"+product.getItem().getItemId();
-        }else if(data instanceof RewardDeliveryEntity) {
+            linkUrl += "/user/donation/detail/" + materialDonation.getDonationId();
+        } else if (data instanceof ProductEntity) {
+            linkUrl = "";
+            // TODO: 마이페이지 상품 결제내역 미구현으로 메인으로 이동 처리
+            //ProductEntity product = (ProductEntity) data;
+            //linkUrl += "/user/product/detail/" + product.getItem().getItemId();
+        } else if (data instanceof RewardDeliveryEntity) {
             RewardDeliveryEntity rewardDelivery = (RewardDeliveryEntity) data;
-            linkUrl += "/rewardDelivery/"+rewardDelivery.getReward().getRewardId();
-        }else if(data instanceof ProductDeliveryEntity) {
-            ProductDeliveryEntity productDelivery = (ProductDeliveryEntity) data;
-            linkUrl += "/productDelivery/"+productDelivery.getProduct().getItem().getItemId();
+            linkUrl += "/user/donation/detail/" + rewardDelivery.getDonation().getDonationId();
+        } else if (data instanceof ProductDeliveryEntity) {
+            linkUrl = "";
+            // TODO: 마이페이지 상품 결제내역 미구현으로 메인으로 이동 처리
+            // ProductDeliveryEntity productDelivery = (ProductDeliveryEntity) data;
+            // linkUrl += "/productDelivery/" + productDelivery.getProduct().getItem().getItemId();
         }
         return linkUrl;
+    }
+
+    // 알림 이미지 URL 생성
+    public String generateImageUrl(Object data){
+        if(data instanceof NoticeEntity){
+            return imageUrlService.findImage(0L);   //0번이 기본 이미지
+        }else if(data instanceof CampaignEntity){
+            return imageUrlService.findImage(((CampaignEntity) data).getPhoto().getId());
+        }else if(data instanceof CampaignBoardEntity){
+            return imageUrlService.findImage(((CampaignBoardEntity) data).getCampaign().getPhoto().getId());
+        }else if(data instanceof BadgeEntity){
+            return imageUrlService.findImage(((BadgeEntity) data).getPhoto().getId());
+        }else if(data instanceof MaterialDonationEntity){
+            return imageUrlService.findImage(((MaterialDonationEntity) data).getCampaign().getPhoto().getId());
+        }else if(data instanceof ProductEntity){
+            return imageUrlService.findImage(((ProductEntity) data).getItem().getPhoto().getId());
+        }else{
+            throw new IllegalArgumentException("[이미지 URL 생성]지원하지 않는 데이터 타입 : " + data.getClass().getSimpleName());
+        }
     }
 
 }
--- ufc/src/main/java/app/scit46/ufc/service/alert/UserAlertService.java
+++ ufc/src/main/java/app/scit46/ufc/service/alert/UserAlertService.java
@@ -35,21 +35,45 @@ public class UserAlertService {
         return userAlertRepository.findByUserAndReadTimeIsNull(user).stream().map(UserAlertDTO::toDTO).collect(Collectors.toList());
     }
 
+    // 사용자가 받은 알림 조회
     public List<UserAlertDTO> getUserAlertList(Long userId) {
         return userAlertRepository.findByUser_UserIdOrderByCreatedTimeDesc(userId).stream().map(UserAlertDTO::toDTO).collect(Collectors.toList());
     }
 
     // 작성된 알림과 사용자 아이디를 받아서 알림 전송
-    public void sendAlert(Long userId, String alertType, AlertEntity data) {
-        UserEntity user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("알림을 받을 사용자가 존재하지 않습니다 UID:"+userId));
-
-        userAlertRepository.save(UserAlertEntity.builder()
-            .user(user)
-            .alert(data)
-            .build());
+    public void sendAlert(String alertType, AlertEntity alert, List<UserEntity> users) {
+        switch(alertType){
+            case "Notice":
+                break;
+            case "Campaign":
+                break;
+            case "CampaignBoard":
+                break;
+            case "Badge":
+                break;
+            case "MaterialDonation":
+                break;
+            case "Product":
+                break;
+            case "RewardDelivery":
+                break;
+            case "ProductDelivery":
+                break;
+            default:
+                throw new IllegalArgumentException("지원하지 않는 알림 타입입니다.");
+        }
         
+        // 알림 저장
+        saveAlert(alert, users);
     }
 
-    
+    public void saveAlert(AlertEntity alert, List<UserEntity> users){
+        for (UserEntity user : users) {
+            userAlertRepository.save(UserAlertEntity.builder()
+                    .user(user)
+                    .alert(alert)
+                    .build());
+        }
+    }
 
 }
